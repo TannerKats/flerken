06b9174353c366b562626ec744c883a49650628a
TannerKats <katsarts@dukes.jmu.edu>
2020-02-05 17:58:35 -0500

    First Commit

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,12 @@
+i = 0
+
+while i < 1001:
+    if i % 15 == 0:
+        print ("FizzBuzz")
+    elif i % 5 == 0:
+        print ("Buzz")
+    elif i % 3 == 0:
+        print ("Fizz")
+    else:
+        print (i)
+    i += 1
