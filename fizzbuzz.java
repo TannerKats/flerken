88f31a2df605ac32ec157713c3e84ecff2626fbf
TannerKats <katsarts@dukes.jmu.edu>
2020-02-04 20:07:12 -0500

    First commit

---
+++ fizzbuzz.java
@@ -0,0 +1,32 @@
+public class fizzbuzz {
+    public static void main(String args[]) {
+
+    int x = 1;
+
+        while (x < 101) {
+            if (x % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (x % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (x % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(x);
+            }
+            x++;
+        }
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
